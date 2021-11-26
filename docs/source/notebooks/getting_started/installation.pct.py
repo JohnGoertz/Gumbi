26436762665ed9aeacc048fc18fb558569f57ea7
johngoertz <johngoertz@gmail.com>
2021-11-26 11:47:45 +0000

    Updates installation notebook

--- docs/source/notebooks/getting_started/installation.pct.py
+++ docs/source/notebooks/getting_started/installation.pct.py
@@ -3,12 +3,12 @@
 #   jupytext:
 #     cell_metadata_filter: -all
 #     formats: ipynb,.pct.py:percent
+#     notebook_metadata_filter: kernelspec
 #     text_representation:
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.11.5
-#     notebook_metadata_filter: kernelspec
+#       jupytext_version: 1.13.1
 # ---
 
 # %% [markdown]
