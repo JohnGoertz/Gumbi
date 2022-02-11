722d50a42f15fb59d677921aa519e0e44cb2ae2b
johngoertz <johngoertz@gmail.com>
2022-02-11 15:45:20 +0000

    Updates requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 import pathlib as pl
 
-VERSION = '0.1.6'
+VERSION = '0.1.7'
 DISTNAME = "gumbi"
 DESCRIPTION = "Gaussian Process Model Building Interface"
 AUTHOR = "John Goertz"
